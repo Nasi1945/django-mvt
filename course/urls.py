7720b3b3339bfc1d0932081721bff97a7adb0bdd
nasi_day <157891907+Nasi1945@users.noreply.github.com>
2024-08-09 14:20:27 +0330

    edit view

--- course/urls.py
+++ course/urls.py
@@ -5,6 +5,9 @@ app_name = 'course'
 
 urlpatterns = [
     path('',courses , name = 'course'),
+    path('category/<str:catname>',courses , name = 'course_category'),
+    path('trainer/<str:trainer>',courses , name = 'course_trainer'),
+    path('price/<str:price>',courses , name = 'course_price'),
+    path('details/<int:id>',course_details , name = 'course_details'),
     path('trainers/',trainers , name = 'trainers'),
-    path('details',details , name= 'details'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Courses,Trainers
 
-def courses(requests):
-    if requests.GET.get('catname'):
-        course = Courses.objects.filter(category__name = requests.GET.get('catname'))
+def courses(requests,**kwargs):
+    if requests.GET.get('search') is not None:
+        course = Courses.objects.filter(content__contains = requests.GET.get('search'))
+    elif kwargs.get('catname'):
+        course = Courses.objects.filter(category__name = kwargs.get('catname'))
+#    if requests.GET.get('catname'):
+#           course = Courses.objects.filter(category__name = requests.GET.get('catname'))
+    elif kwargs.get('trainer'):
+        course = Courses.objects.filter(trainers__user__username = kwargs.get('trainer'))
+    elif kwargs.get('price'):
+        course = Courses.objects.filter(fee__lte = float(kwargs.get('price')))
     else:
         course = Courses.objects.filter(status = True)
     context = {
@@ -18,6 +26,10 @@ def trainers(requests):
     }
     return render(requests,'course/trainers.html',context=context)
 
-def details(requests):
-    return render(requests,'course/course-details.html')
+def course_details(requests , id):
+    cours = Courses.objects.get(id=id)
+    context = {
+        'cours':cours
+    }
+    return render(requests,'course/course-details.html',context=context)
 
